7980231543854af6e5ec2506e2b74cf0db869e69
KJR020 <johnjiro1114@gmail.com>
2024-04-22 06:04:35 +0900

    test: Add unit test for home page view in lists/tests.py

--- lists/tests.py
+++ lists/tests.py
@@ -1,9 +1,19 @@
 from django.test import TestCase
+from django.http import HttpRequest
+from lists.views import home_page
 
-# Create your tests here.
 
-
-class SmokeTest(TestCase):
-
-    def test_bad_maths(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_home_page_returns_correct_html(self):
+        """
+        requestオブジェクトを作成し、home_pageビュー関数に渡す。
+        これにより、home_pageのURLを叩いたときに返ってくるレスポンスを取得できる。
+        Responseオブジェクトからcontent属性を取得し、その中身を文字列に変換する。
+        その文字列が、<html>で始まり、</html>で終わることを確認する。
+        """
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode("utf8")
+        self.assertIn("<title>Tod-Do lists</title>", html)
+        self.assertTrue(html.startswith("<html>"))
+        self.assertTrue(html.endswith("</html>"))
