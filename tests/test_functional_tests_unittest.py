b567450f69a79705214dab03d5a930df05d01958
KJR020 <johnjiro1114@gmail.com>
2024-04-22 06:57:15 +0900

    Update functional test in tests/test_functional_tests_unittest.py

--- tests/test_functional_tests_unittest.py
+++ tests/test_functional_tests_unittest.py
@@ -1,4 +1,7 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 
@@ -11,24 +14,42 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
+        """
+        seleniumを使って、タグ名、id属性、テキストを使って要素を検索する
+        send_keysメソッドを使って、テキストボックスにテキストを入力する
+        Keys.ENTERを使って、テキストボックスに入力したテキストを送信する
+        time.sleepメソッドを使って、ページが更新されるまで待機する。`explicit wait`と呼ばれるらしい。
+
+        """
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
         self.browser.get("http://localhost:8000")
 
         # She notices the page title and header mention to-do lists
         self.assertIn("To-Do", self.browser.title)
-        self.fail("Finish the test!")
+        header_text = self.browser.find_element(By.TAG_NAME, "h1").text
+        self.assertIn("To-Do", header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element(By.Id, "id_new_item")
+        self.assertEqual(inputbox.get_attribute("placeholder"), "Enter a to-do item")
 
         # She types "Buy peacock feathers" into a text box (Edith's hobby
         # is tying fly-fishing lures)
+        inputbox.send_keys("Buy peacock feathers")
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element(By.Id, "id_list_table")
+        rows = self.browser.find_elements(By.TAG_NAME, "tr")
+        self.assertTrue(any(row.text == "1: But peacock feathers" for row in rows))
 
         # There is still a text box inviting her to add another item. She
         # enters "Use peacock feathers to make a fly" (Edith is very methodical)
+        self.fail("Finish the test!")
 
         # The page updates again, and now shows both items on her list
 
