2a2d92004e39796277d0d7b7ec721409176b53b6
KJR020 <johnjiro1114@gmail.com>
2024-04-23 22:26:27 +0900

    refactor: Refactor home page view to use a template

--- lists/tests.py
+++ lists/tests.py
@@ -4,12 +4,10 @@ from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_home_page_returns_correct_html(self):
+    def test_uses_home_template(self):
         """
         clientオブジェクトを使って、home_pageビュー関数にリクエストを送信し、レスポンスを取得する
         responseオブジェクトのcontent属性を使わずに、assertContainsメソッドを使用
         """
         response = self.client.get("/")
-        self.assertContains(response, "<title>To-Do lists</title>")
-        self.assertContains(response, "<html>")
-        self.assertContains(response, "</html>")
+        self.assertTemplateUsed(response, "home.html")
